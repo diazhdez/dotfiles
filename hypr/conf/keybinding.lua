9b9b7fab7ec2bf068ee4f6f0f1d8543d115df407
Josue Diaz Hernandez <jd5531066@gmail.com>
2026-06-12 19:10:09 -0600

    refactor: switch from whitelist to blacklist .gitignore

--- hypr/conf/keybinding.lua
+++ hypr/conf/keybinding.lua
@@ -14,7 +14,7 @@ hl.bind(mainMod .. " + O", hl.dsp.exec_cmd("~/.config/scripts/power_menu.sh"))
 -- Apps
 hl.bind(mainMod .. " + Space", hl.dsp.exec_cmd("ghostty"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd("ghostty -e yazi"))
-hl.bind(mainMod .. " + T", hl.dsp.exec_cmd("ghostty -e nvim"))
+hl.bind(mainMod .. " + T", hl.dsp.exec_cmd("ghostty -e micro"))
 hl.bind(mainMod .. " + B", hl.dsp.exec_cmd("zen-browser"))
 hl.bind(mainMod .. " + P", hl.dsp.exec_cmd("spotify"))
 hl.bind(mainMod .. " + A", hl.dsp.exec_cmd("ghostty --title=wiremix -e wiremix"))
@@ -24,7 +24,7 @@ hl.bind(mainMod .. " + SHIFT + Q", hl.dsp.window.close())
 hl.bind(mainMod .. " + SHIFT + F", hl.dsp.window.float({ action = "toggle" }))
 
 -- Utilities
-hl.bind(mainMod .. " + SHIFT + L", hl.dsp.exec_cmd("hyprlock"))
+hl.bind(mainMod .. " + SHIFT + M", hl.dsp.exec_cmd("hyprlock"))
 hl.bind(mainMod .. " + SHIFT + S", hl.dsp.exec_cmd("hyprshot -m region -o $HOME/Pictures/Screenshots"))
 hl.bind(mainMod .. " + V", hl.dsp.exec_cmd("~/.config/scripts/clipboard_launcher.sh"))
 hl.bind(mainMod .. " + F", hl.dsp.exec_cmd("~/.config/scripts/wallpaper_select.sh"))
@@ -36,6 +36,12 @@ hl.bind(mainMod .. " + H", hl.dsp.focus({ direction = "left" }))
 hl.bind(mainMod .. " + J", hl.dsp.focus({ direction = "down" }))
 hl.bind(mainMod .. " + K", hl.dsp.focus({ direction = "up" }))
 hl.bind(mainMod .. " + L", hl.dsp.focus({ direction = "right" }))
+
+-- Move windows
+hl.bind(mainMod .. " + SHIFT + H", hl.dsp.window.move({ direction = "left" }))
+hl.bind(mainMod .. " + SHIFT + J", hl.dsp.window.move({ direction = "down" }))
+hl.bind(mainMod .. " + SHIFT + K", hl.dsp.window.move({ direction = "up" }))
+hl.bind(mainMod .. " + SHIFT + L", hl.dsp.window.move({ direction = "right" }))
 
 -- Workspaces
 for i = 1, 10 do
